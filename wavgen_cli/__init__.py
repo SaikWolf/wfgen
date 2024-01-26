1cb584c2a3eaa137b15f61dbf905680938f5b7f2
Bill Clark <saikou@restdnn>
2024-01-26 15:38:13 -0500

    Incorporating the initial cli python stuff.

---
+++ wavgen_cli/__init__.py
@@ -0,0 +1,18 @@
+
+
+from .utils import encoder,decoder,MultiSocket,have_pygr,have_freqhopper
+from . import server,client,utils,profiles
+from .launcher import launch
+from .c_logger import logger_client,fake_log,logger as c_logger
+
+BaseAudio = None
+import os
+if 'dgrl_preload_audio' in os.environ and os.environ["dgrl_preload_audio"]:
+    if BaseAudio is None:
+        print("Preloading audio into BaseAudio")
+        BaseAudio = profiles.analogs.AudioCombiner(profiles.analogs.get_audio_files())
+        print("Preloading complete")
+del os
+
+if have_pygr():
+    from .utils import gen_pmt_header,parse_pmt_header
